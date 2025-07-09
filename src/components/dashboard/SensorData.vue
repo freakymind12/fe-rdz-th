d9f6396e84b9e493fae2220ea304da133edf80a0
freakymind12 <riankurniawan132@gmail.com>
2025-07-09 10:34:32 +0700

    refactor: add sensorData component and refactor liveDashboard

---
+++ src/components/dashboard/SensorData.vue
@@ -0,0 +1,132 @@
+<template>
+  <!-- Temporary condition waiting update sensor payload -->
+  <a-flex vertical align="center" class="wrapper-data" v-if="value != 0 || null" :gap="0">
+    <span class="bold">{{ sensorLabel }}</span>
+    <transition name="zoom" mode="out-in">
+      <span class="x-large bold" :class="{ 'ng-sensor': isOutOfRange }" :key="value">
+        <component :is="sensorDisplay ? 'span' : LoadingOutlined" v-bind="sensorDisplay ? {} : { spin: true }">
+          {{ sensorDisplay }}
+        </component>
+      </span>
+    </transition>
+
+    <a-flex vertical :gap="0" wrap="wrap" align="center">
+      <span>Min : {{ min || '-' }} {{ sensorDisplay?.split(' ').pop() }}</span>
+      <span>Max : {{ max || '-' }} {{ sensorDisplay?.split(' ').pop() }}</span>
+    </a-flex>
+
+    <a-flex justify="space-between" gap="small" wrap="wrap" align="center">
+      <a-popover placement="bottom">
+        <template #title>
+          <span>Sensor Message</span>
+        </template>
+        <template #content>
+          <template v-if="isError">
+            <span v-for="(msg, index) in errorReasons" :key="index">{{ msg }}</span>
+          </template>
+          <template v-else>
+            <span>There is no error message</span>
+          </template>
+        </template>
+        <a-tag class="bold" :color="sensorColor">
+          {{ isError ? 'Error' : 'Normal' }}
+        </a-tag>
+      </a-popover>
+    </a-flex>
+  </a-flex>
+</template>
+
+<script setup>
+import { LoadingOutlined } from '@ant-design/icons-vue'
+import { useWebSocketStore } from '@/stores/websocket'
+import { computed } from 'vue'
+
+const wsStore = useWebSocketStore()
+
+const props = defineProps({
+  device: Object,
+  sensorKey: String, // contoh: 'sensor.temp'
+  sensorLabel: {
+    type: String,
+    default: 'Sensor'
+  },
+  minKey: String, // contoh: 'sensor.t_min'
+  maxKey: String, // contoh: 'sensor.t_max'
+  errorPath: String, // contoh: 'error.temperature.isError'
+  reasonPath: String, // contoh: 'error.temperature.reason'
+})
+
+// Access Websocket Data
+const getWsData = (area, key) => {
+  if (!area || !key) return null
+
+  const deviceData = wsStore.data.find((d) => d.area === area)
+  if (!deviceData) return null
+
+  // Helper untuk akses nested path seperti 'sensor.temp'
+  const getNestedValue = (obj, path) => {
+    return path
+      .split('.')
+      .reduce((acc, part) => {
+        if (acc && Object.prototype.hasOwnProperty.call(acc, part)) {
+          return acc[part]
+        }
+        return undefined
+      }, obj)
+  }
+
+  const result = getNestedValue(deviceData, key)
+  return result !== undefined ? result : null
+}
+
+const value = computed(() => getWsData(props.device.area, props.sensorKey))
+const min = computed(() => getWsData(props.device.area, props.minKey))
+const max = computed(() => getWsData(props.device.area, props.maxKey))
+const isOutOfRange = computed(() => {
+  return value.value <= min.value || value.value >= max.value
+})
+
+// Get & Format Sensor Data
+const sensorDisplay = computed(() => {
+  const units = {
+    'sensor.temp': '°C',
+    'sensor.humi': '%',
+    'sensor.pressure': 'Pa',
+    'sensor.noise': 'dB',
+  }
+  if (value.value != null) {
+    return `${value.value} ${units[props.sensorKey] || ''}`
+  }
+  return null
+})
+const isError = computed(() => getWsData(props.device.area, props.errorPath))
+const sensorColor = computed(() => isError.value ? '#AC1754' : '#3D8D7A')
+const errorReasons = computed(() => getWsData(props.device.area, props.reasonPath) || [])
+</script>
+
+<style scoped>
+.wrapper-data {
+  padding: 0.5rem;
+}
+
+.ng-sensor {
+  color: red;
+}
+
+.zoom-enter-active,
+.zoom-leave-active {
+  transition:
+    transform 0.3s ease,
+    opacity 0.3s ease;
+}
+
+.zoom-enter-from {
+  transform: scale(0);
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  transform: scale(0);
+  opacity: 0;
+}
+</style>
--- src/views/dashboard/LiveDashboard.vue
+++ src/views/dashboard/LiveDashboard.vue
@@ -1,22 +1,12 @@
 <template>
   <div>
-    <a-flex
-      justify="space-between"
-      align="center"
-      style="margin-bottom: 1rem"
-      wrap="wrap"
-      gap="small"
-    >
+    <a-flex justify="space-between" align="center" style="margin-bottom: 1rem" wrap="wrap" gap="small">
       <span class="bold x-large">Temperature & Humidity Monitor </span>
 
       <a-space :size="0" direction="vertical">
         <span class="bold">Group</span>
-        <a-select
-          style="width: 200px;"
-          :options="groupOptions"
-          v-model:value="wsStore.selectedGroup"
-          @change="wsStore.handleChangeGroup"
-        />
+        <a-select style="width: 200px;" :options="groupOptions" v-model:value="wsStore.selectedGroup"
+          @change="wsStore.handleChangeGroup" />
       </a-space>
     </a-flex>
     <a-result v-if="wsStore.filterDeviceByGroup.length === 0" class="no-device">
@@ -28,18 +18,8 @@
       </template>
     </a-result>
     <a-row :gutter="[16, 16]">
-      <BaseCardColumn
-        :xs="24"
-        :md="12"
-        :sm="12"
-        :lg="12"
-        :xl="6"
-        size="small"
-        :bordered="true"
-        v-for="device in wsStore.filterDeviceByGroup"
-        :key="device.area"
-        :hoverable="true"
-      >
+      <BaseCardColumn :xs="24" :md="12" :sm="24" :lg="12" :xl="6" size="small" :bordered="true"
+        v-for="device in wsStore.filterDeviceByGroup" :key="device.area" :hoverable="true">
         <template #body>
           <a-spin :spinning="device.disconnected">
             <template #tip>
@@ -48,124 +28,44 @@
                 <span class="bold">Waiting data from this device</span>
               </a-space>
             </template>
+            <!-- Device Area & Last Updated -->
             <a-flex justify="space-between" align="start" wrap="wrap">
               <a-space direction="vertical" :size="1">
-                <span class="area">{{ formatArea(device.area) }}</span>
-                <a-tag :color="device.group_name ? '#3D8D7A' : '#AC1754'" class="bold">
-                  {{ device.group_name || 'Ungrouped' }}</a-tag
-                >
+                <span class="x-large bold">{{ formatArea(device.area) }}</span>
+                <a-space :size="0">
+                  <a-tag :color="device.group_name ? '#3D8D7A' : '#AC1754'" class="bold">
+                    {{ device.group_name || 'Ungrouped' }}</a-tag>
+                  <a-tag color="grey" class="bold"> {{ getWsData(device.area, 'other.th_id') || 'Unknown' }}</a-tag>
+                </a-space>
               </a-space>
-              <a-flex vertical class="last-updated">
+              <a-flex vertical class="small">
                 <span class="bold">Last Updated</span>
                 <span> {{ getWsData(device.area, 'date') }}</span>
               </a-flex>
             </a-flex>
-            <a-flex justify="space-around" align="center" wrap="wrap">
-              <a-flex vertical align="center" class="wrapper-data">
-                <span>Temperature </span>
-                <transition name="zoom" mode="out-in">
-                  <span
-                    class="area"
-                    :class="{
-                      'ng-sensor':
-                        getWsData(device.area, 'sensor.temp') <=
-                          getWsData(device.area, 'sensor.t_min') ||
-                        getWsData(device.area, 'sensor.temp') >=
-                          getWsData(device.area, 'sensor.t_max'),
-                    }"
-                    :key="getWsData(device.area, 'sensor.temp')"
-                  >
-                    {{ getWsData(device.area, 'sensor.temp') }} °C
-                  </span>
-                </transition>
-                <a-flex justify="space-between" gap="small" wrap="wrap" align="center">
-                  <span>Min : {{ getWsData(device.area, 'sensor.t_min') }} °C</span>
-                  <span>Max : {{ getWsData(device.area, 'sensor.t_max') }} °C</span>
-                </a-flex>
-                <a-flex justify="space-between" gap="small" wrap="wrap" align="center">
-                  <!-- <span>Status : </span> -->
-                  <a-popover placement="bottom">
-                    <template #title>
-                      <span>Sensor Message</span>
-                    </template>
-                    <template #content v-if="getWsData(device.area, 'error.temperature.isError')">
-                      <span
-                        v-for="(msg, index) in getWsData(device.area, 'error.temperature.reason')"
-                        :key="index"
-                      >
-                        {{ msg }}
-                      </span>
-                    </template>
-                    <template #content v-else>
-                      <span>There is no error message</span>
-                    </template>
-                    <a-tag
-                      class="bold"
-                      :color="
-                        getWsData(device.area, 'error.temperature.isError') ? '#AC1754' : '#3D8D7A'
-                      "
-                    >
-                      {{ getWsData(device.area, 'error.temperature.isError') ? 'Error' : 'Normal' }}
-                    </a-tag>
-                  </a-popover>
-                </a-flex>
-              </a-flex>
-              <a-flex vertical align="center" class="wrapper-data">
-                <span>Humidity </span>
-                <transition name="zoom" mode="out-in">
-                  <span
-                    class="area"
-                    :key="getWsData(device.area, 'sensor.humi')"
-                    :class="{
-                      'ng-sensor':
-                        getWsData(device.area, 'sensor.humi') <=
-                          getWsData(device.area, 'sensor.h_min') ||
-                        getWsData(device.area, 'sensor.humi') >=
-                          getWsData(device.area, 'sensor.h_max'),
-                    }"
-                  >
-                    {{ getWsData(device.area, 'sensor.humi') }} %
-                  </span>
-                </transition>
-                <a-flex justify="space-between" gap="small" wrap="wrap" align="center">
-                  <span>Min : </span> {{ getWsData(device.area, 'sensor.h_min') }} %
-                  <span>Max :</span> {{ getWsData(device.area, 'sensor.h_max') }} %
-                </a-flex>
-                <a-flex justify="space-between" gap="small" wrap="wrap" align="center">
-                  <!-- <span>Status : </span> -->
-                  <a-popover placement="bottom">
-                    <template #title>
-                      <span>Sensor Message</span>
-                    </template>
-                    <template #content v-if="getWsData(device.area, 'error.humidity.isError')">
-                      <span
-                        v-for="(msg, index) in getWsData(device.area, 'error.humidity.reason')"
-                        :key="index"
-                      >
-                        {{ msg }}
-                      </span>
-                    </template>
-                    <template #content v-else>
-                      <span>There is no error message</span>
-                    </template>
-                    <a-tag
-                      class="bold"
-                      :color="
-                        getWsData(device.area, 'error.humidity.isError') ? '#AC1754' : '#3D8D7A'
-                      "
-                    >
-                      {{ getWsData(device.area, 'error.humidity.isError') ? 'Error' : 'Normal' }}
-                    </a-tag>
-                  </a-popover>
-                </a-flex>
-              </a-flex>
+            <!-- Wrapper Sensor -->
+            <a-flex justify="space-evenly" align="center" wrap="wrap">
+              <!-- Temperature Sensor Data -->
+              <SensorData :device="device" sensorKey="sensor.temp" sensorLabel="Temperature" minKey="sensor.t_min"
+                maxKey="sensor.t_max" errorPath="error.temperature.isError" reasonPath="error.temperature.reason"/>
+              <!-- Humidity Sensor Data -->
+              <SensorData :device="device" sensorKey="sensor.humi" sensorLabel="Humidity" minKey="sensor.h_min"
+                maxKey="sensor.h_max" errorPath="error.humidity.isError" reasonPath="error.humidity.reason" />
+              <!-- Noise Sensor Data -->
+              <SensorData :device="device" sensorKey="sensor.noise" sensorLabel="Noise" minKey="sensor.n_min"
+                maxKey="sensor.n_max" errorPath="error.noise.isError" reasonPath="error.noise.reason" />
+              <!-- Pressure Sensor Data -->
+              <SensorData :device="device" sensorKey="sensor.pressure" sensorLabel="Pressure" minKey="sensor.p_min"
+                maxKey="sensor.p_max" errorPath="error.pressure.isError" reasonPath="error.pressure.reason" />
             </a-flex>
+            <!-- Wrapper Calibration & Device Setting -->
             <a-flex justify="space-around" align="start" wrap="wrap">
+              <!-- Calibration Data -->
               <a-flex vertical class="wrapper-data">
                 <span class="small">Calibration Date : </span>
                 <a-flex justify="space-between" gap="small" wrap="wrap" align="center">
-                  <a-tag color="grey" class="bold"
-                    >{{ dayjs(getWsData(device.area, 'other.calibration')).format('YYYY-MM-DD') }}
+                  <a-tag color="grey" class="bold">{{ dayjs(getWsData(device.area,
+                    'other.calibration')).format('YYYY-MM-DD') }}
                   </a-tag>
                   <a-tag color="grey" class="bold">
                     {{
@@ -178,15 +78,12 @@
                   </a-tag>
                 </a-flex>
               </a-flex>
+              <!-- Device Setting -->
               <a-flex vertical class="wrapper-data">
                 <span class="small">Device Setting : </span>
                 <a-flex justify="space-between" gap="small" wrap="wrap" align="center">
-                  <a-tag color="#205781" class="bold" @click="handleModal(device, 'config')"
-                    >Configuration</a-tag
-                  >
-                  <a-tag color="#205781" class="bold" @click="handleModal(device, 'rules')"
-                    >Rules</a-tag
-                  >
+                  <a-tag color="#205781" class="bold" @click="handleModal(device, 'config')">Configuration</a-tag>
+                  <a-tag color="#205781" class="bold" @click="handleModal(device, 'rules')">Rules</a-tag>
                 </a-flex>
               </a-flex>
             </a-flex>
@@ -205,6 +102,7 @@ import { useDashboardStore } from '@/stores/dashboard'
 import { useWebSocketStore } from '@/stores/websocket'
 import { useGroupStore } from '@/stores/group'
 import { onMounted, onUnmounted, ref, watch } from 'vue'
+import SensorData from '@/components/dashboard/SensorData.vue'
 
 import dayjs from 'dayjs'
 
@@ -213,6 +111,7 @@ const dashboardStore = useDashboardStore()
 const groupStore = useGroupStore()
 const groupOptions = ref()
 
+// Handle modal operation
 const handleModal = (data, mode) => {
   const mappedData = {
     area: data.area,
@@ -229,6 +128,7 @@ const handleModal = (data, mode) => {
   dashboardStore.modalVisible = true
 }
 
+// Access Websocket Data
 const getWsData = (area, key) => {
   const deviceData = wsStore.data.find((d) => d.area === area)
 
@@ -238,7 +138,7 @@ const getWsData = (area, key) => {
   const getNestedValue = (obj, path) => {
     return path
       .split('.')
-      .reduce((acc, part) => (acc && acc[part] !== undefined ? acc[part] : '-'), obj)
+      .reduce((acc, part) => (acc && acc[part] !== undefined ? acc[part] : undefined), obj)
   }
 
   return getNestedValue(deviceData, key)
@@ -295,47 +195,7 @@ watch(
   padding-top: 15%;
 }
 
-.area {
-  font-size: x-large;
-  font-weight: bold;
-}
-
-.ng-sensor {
-  color: red;
-}
-
-.bold {
-  font-weight: bold;
-}
-
-.last-updated {
-  font-size: small;
-}
-
 .wrapper-data {
   padding: 0.5rem;
-}
-
-.zoom-enter-active,
-.zoom-leave-active {
-  transition:
-    transform 0.3s ease,
-    opacity 0.3s ease;
-}
-.zoom-enter-from {
-  transform: scale(0);
-  opacity: 0;
-}
-.zoom-leave-to {
-  transform: scale(0);
-  opacity: 0;
-}
-
-.small {
-  font-size: small;
-}
-
-.x-large {
-  font-size: x-large;
 }
 </style>
